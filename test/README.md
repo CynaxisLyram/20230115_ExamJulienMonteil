60fef482701ca49f66081bdec5fdc5d8d1666728
CynaxisLyram <julien.monteil81@gmail.com>
2024-01-15 11:43:22 +0100

    tests des codes de Steve et Turing

---
+++ test/README.md
@@ -0,0 +1,6 @@
+Monteil Julien
+version 0.0.0
+15/01/2023
+
+Toutes les fonctions de Turing passent les tests.
+Les fonctions décomposer et somme de Steve ne passent pas les tests.
---
+++ test/test_steve.py
@@ -0,0 +1,18 @@
+from devine_steve import *
+def test_decomposer():
+    assert (decomposer(0)) == None
+    assert (decomposer(99)) == None
+    assert (decomposer(1000)) == None
+    assert (decomposer(10000)) == None
+    assert (decomposer(100)) == (1,0,0)
+    assert (decomposer(555)) == (5,5,5)
+    assert (decomposer(999)) == (9,9,9)
+
+def test_divisible():
+    assert (divisible(180)) == True
+    assert (divisible(200)) == False
+
+def test_somme():
+    assert (somme(3,9,6)) == True
+    assert (somme(2,5,3)) == True
+    assert (somme(3,3,3)) == False
